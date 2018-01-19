9a70a4f68351cb7f46aa8633b66f64941e1c5d24
Guillermo Orellana <wiyarmir@gmail.com>
2018-01-19 17:39:32 +0100

    replace common extensions with dep (still coroutines there tho)

--- web/src/main/kotlin/common/jsdom.kt
+++
@@ -1,8 +0,0 @@
-package org.jetbrains.common
-
-import org.w3c.dom.*
-import org.w3c.dom.events.Event
-
-internal val Event.inputValue: String
-    get() = (target as? HTMLInputElement)?.value ?: (target as? HTMLTextAreaElement)?.value ?: ""
-
--- web/src/main/kotlin/es/guillermoorellana/keynotedex/web/main.kt
+++ web/src/main/kotlin/es/guillermoorellana/keynotedex/web/main.kt
@@ -1,11 +1,12 @@
 package es.guillermoorellana.keynotedex.web
 
 import es.guillermoorellana.keynotedex.web.components.*
+import kotlinext.js.*
 import react.dom.render
 import kotlin.browser.*
 
 fun main(args: Array<String>) {
-    runtime.wrappers.require("narrow-jumbotron.css")
+    require("narrow-jumbotron.css")
 
     window.onload = {
         render(document.getElementById("content")) {
