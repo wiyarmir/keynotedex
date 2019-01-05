877855bf6fd1c6f49c1dd91b14a712574d15368f
Guillermo Orellana <wiyarmir@gmail.com>
2019-01-05 14:06:36 +0100

    This class is now superfluous

--- backend/src/main/kotlin/es/guillermoorellana/keynotedex/backend/Server.kt
+++
@@ -1,9 +0,0 @@
-package es.guillermoorellana.keynotedex.backend
-
-import io.ktor.server.engine.commandLineEnvironment
-import io.ktor.server.engine.embeddedServer
-import io.ktor.server.netty.Netty
-
-fun main(args: Array<String>) {
-    embeddedServer(Netty, commandLineEnvironment(args)).start()
-}
