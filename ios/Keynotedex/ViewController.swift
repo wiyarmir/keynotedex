5e383d148a848a4c9d8f7914e171725788c590d2
Guillermo Orellana <guillermo.orellana@skyscanner.net>
2019-04-27 14:39:11 +0100

    Change config

--- ios/Keynotedex/ViewController.swift
+++ ios/Keynotedex/ViewController.swift
@@ -12,9 +12,13 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let label = UILabel(frame: CGRect(x: 0, y: 0, width: 300, height: 21))
+        label.center = CGPoint(x: 160, y: 285)
+        label.textAlignment = .center
+        label.font = label.font.withSize(25)
+        label.text = "Hello"
+        view.addSubview(label)
     }
-
-
+    
 }
 
