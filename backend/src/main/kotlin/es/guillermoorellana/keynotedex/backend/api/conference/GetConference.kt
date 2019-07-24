4ca5f703fae070c35cf316d869cc776d410e9533
Guillermo Orellana <wiyarmir@gmail.com>
2019-07-24 16:52:43 +0100

    Expose list of conferences in the API

--- backend/src/main/kotlin/es/guillermoorellana/keynotedex/backend/api/conference/GetConference.kt
+++ backend/src/main/kotlin/es/guillermoorellana/keynotedex/backend/api/conference/GetConference.kt
@@ -5,6 +5,7 @@ import es.guillermoorellana.keynotedex.backend.data.conferences.ConferenceStorag
 import es.guillermoorellana.keynotedex.backend.data.conferences.toDto
 import es.guillermoorellana.keynotedex.backend.data.hashids
 import es.guillermoorellana.keynotedex.datasource.responses.ConferenceResponse
+import es.guillermoorellana.keynotedex.datasource.responses.ConferencesResponse
 import es.guillermoorellana.keynotedex.datasource.responses.ErrorResponse
 import io.ktor.application.call
 import io.ktor.http.ContentType
@@ -19,11 +20,16 @@ import io.ktor.routing.accept
 fun Route.getConference(conferenceStorage: ConferenceStorage) {
 
     JsonSerializableConverter.register(ConferenceResponse.serializer())
+    JsonSerializableConverter.register(ConferencesResponse.serializer())
 
     accept(ContentType.Application.Json) {
         get<ConferenceEndpoint> { (conferenceId) ->
+            if (conferenceId == null) {
+                call.respond(ConferencesResponse(conferenceStorage.conferences().toDto()))
+                return@get
+            }
             val conference = conferenceId
-                ?.let { hashids.decode(it).firstOrNull() }
+                .let { hashids.decode(it).firstOrNull() }
                 ?.let { conferenceStorage.conference(it) }
             when (conference) {
                 null -> call.respond(HttpStatusCode.NotFound, ErrorResponse("Not found"))
--- backend/src/main/kotlin/es/guillermoorellana/keynotedex/backend/data/conferences/conferenceTransforms.kt
+++ backend/src/main/kotlin/es/guillermoorellana/keynotedex/backend/data/conferences/conferenceTransforms.kt
@@ -2,12 +2,15 @@ package es.guillermoorellana.keynotedex.backend.data.conferences
 
 import org.jetbrains.squash.results.ResultRow
 import org.jetbrains.squash.results.get
+import es.guillermoorellana.keynotedex.datasource.dto.Conference as DtoConference
 
 fun transformConference(it: ResultRow): Conference =
     Conference(
         name = it[ConferencesTable.name]
     )
 
-fun Conference.toDto() = es.guillermoorellana.keynotedex.datasource.dto.Conference(
+fun Conference.toDto() = DtoConference(
     name = name
 )
+
+fun List<Conference>.toDto() = map { it.toDto() }
